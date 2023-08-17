915d72028f726bf00dba1e99ba43bff60a9953ce
Dammymure <dammymure1@gmail.com>
2023-08-17 17:06:14 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -20,8 +20,7 @@ import Options from './components/Options';
 
 function App() {
   const { setUserInfo, userInfo, auth, setAuth } = useContext(UserContext);
-  const allow = localStorage.getItem("currentUser")
-
+  // const [auth, setAuth] = useState()
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}/user/profile`, {
       method: 'GET',
@@ -44,6 +43,7 @@ function App() {
 
   // const username = userInfo?.token
   const username = auth.token
+  const allow = localStorage.getItem("currentUser")
   return (
     <UserContextProvider>
       <Routes>
@@ -80,89 +80,3 @@ export function ProtectedRoute(props) {
 }
 
 export default App;
-
-
-
-
-
-
-// import './App.css';
-// import React, { useEffect, useContext } from 'react';
-// import { Routes, Route, Navigate } from "react-router-dom"
-// import IndexPage from "./Pages/IndexPage"
-// import Layout from './Layout';
-// import Tweet from './Pages/Tweet';
-// import Profile from './Pages/Profile';
-// import AllTweets from './Pages/AllTweets';
-// import Login from './Pages/Login';
-// import Error from './Pages/Error';
-// import Register from './Pages/RegisterUser';
-// import { UserContextProvider } from "./UserContext";
-// import LandingPage from './Pages/LandingPage';
-// import { UserContext } from './UserContext';
-// import LeftSide from './components/LeftSide';
-// import News from './components/News';
-// import Options from './components/Options';
-
-// function App() {
-//   const { setUserInfo, setAuth, auth, userInfo } = useContext(UserContext);
-//   const token = localStorage.getItem("currentUser")
-//   useEffect(() => {
-//     ; // Get token from local storage
-
-//     if (token) { // Check if token exists
-//       fetch(`${process.env.REACT_APP_API_URL}/user/profile`, {
-//         method: 'GET',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         credentials: 'include',
-//       }).then(response => {
-//         if (response.ok) {
-//           response.json().then(userInfo => {
-//             setUserInfo(userInfo);
-//             setAuth(userInfo);
-//             console.log(userInfo);
-//             console.log(userInfo.token + "Token");
-//           });
-//         }
-//       });
-//     }
-//   }, [setUserInfo, setAuth]); // Include setUserInfo and setAuth in the dependency array
-
-//   return (
-//     <UserContextProvider>
-//       <Routes>
-//         <Route path='/login' element={<Login />} />
-//         <Route path='/register' element={<Register />} />
-//         <Route path='/' element={<LandingPage />} />
-//         <Route path='/error' element={<Error />} />
-//         <Route path='/' element={<LeftSide />} />
-//         {token && (
-//           <Route path='/home' element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-//             <Route index element={<ProtectedRoute><IndexPage id={userInfo.info.id} /></ProtectedRoute>} />
-//             <Route path='/home/alltweets' element={<ProtectedRoute><AllTweets id={userInfo.info.id} /></ProtectedRoute>} />
-//             <Route path='/home/tweet' element={<ProtectedRoute><Tweet /></ProtectedRoute>} />
-//             <Rout
-//             e path='/home/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-//             <Route path='/home/news' element={<ProtectedRoute><News /></ProtectedRoute>} />
-//             <Route path='/home/options' element={<ProtectedRoute><Options /></ProtectedRoute>} />
-//           </Route>
-//         )}
-//         {!token && (
-//           <Route path='/home/error' element={<Error />} />
-//         )}
-//       </Routes>
-//     </UserContextProvider>
-//   );
-// }
-
-// export function ProtectedRoute(props) {
-//   if (localStorage.getItem("currentUser")) {
-//     return props.children;
-//   } else {
-//     return <Navigate to='/login' />;
-//   }
-// }
-
-// export default App;
